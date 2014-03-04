f4cffc12a3f324d7b04f9b41fc7610730e9fff55
Andy Farkas <afarky@gmail.com>
2014-03-04 14:21:04 +0100

    Moved config files to root

--- Afa/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'ClassLoader' . DIRECTORY_SEPARATOR . 'Loader.php');
-$loader = new \Afa\ClassLoader\Loader();
-$loader->addClasspath(dirname(__DIR__));
-$loader->register();
