0b52c87c35a3f5ac1c066c15a6522f620603a6b9
Andy Farkas <afarky@gmail.com>
2014-03-06 13:27:42 +0100

    removed manual autoload

--- autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(__DIR__ . DIRECTORY_SEPARATOR . 'Afa' . DIRECTORY_SEPARATOR . 'ClassLoader' . DIRECTORY_SEPARATOR . 'Loader.php');
-$loader = new \Afa\ClassLoader\Loader();
-$loader->addClasspath(__DIR__);
-$loader->register();
