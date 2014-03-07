e9e8d1d3e17f42311529c64006184524d24ae4d0
Andy Farkas <afarky@gmail.com>
2014-03-07 16:06:33 +0100

    Implemented simple Delete command

---
+++ src/Afa/Database/Command/Delete.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Afa\Database\Command;
+
+class Delete implements \Afa\Database\ICommand
+{
+
+    /**
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * @var array
+     */
+    protected $where = array();
+
+    /**
+     * @param string $table
+     * @param array $where
+     */
+    public function __construct($table, array $where)
+    {
+        $this->table = $table;
+        $this->where = $where;
+    }
+
+    /**
+     * @param \Afa\Database\IConnection $connection
+     */
+    public function run(\Afa\Database\IConnection $connection)
+    {
+        $columns = array_keys($this->where);
+        $whereString = implode(' AND ', array_map(function($column)
+        {
+            return $column . ' = :' . $column;
+        }, $columns));
+        $sql = sprintf('DELETE FROM %s WHERE %s', $this->table, $whereString);
+        $connection->execute($sql, $this->where);
+    }
+}
