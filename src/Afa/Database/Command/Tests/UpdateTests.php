932d30a827239d668dd7a426a93134d6269ca654
Andy Farkas <afarky@gmail.com>
2014-03-07 16:06:47 +0100

    Added a test for Update command

--- src/Afa/Database/Command/Tests/UpdateTests.php
+++ src/Afa/Database/Command/Tests/UpdateTests.php
@@ -8,7 +8,7 @@ class UpdateTests extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function run_NonEmptyValues_UpdatesDataUsingConnection()
+    public function run_NonEmptyValuesSingleWhereCondition_UpdatesDataUsingConnection()
     {
         $arguments = array(
             'column1' => 1,
@@ -28,6 +28,36 @@ class UpdateTests extends \PHPUnit_Framework_TestCase
             'column2' => 'a',
         ), array(
             'id' => 2,
+        ));
+
+        $command->run($connectionMock);
+    }
+
+    /**
+     * @test
+     */
+    public function run_NonEmptyValuesMultipleWhereConditions_UpdatesDataUsingConnection()
+    {
+        $arguments = array(
+            'column1' => 1,
+            'column2' => 'a',
+            'id' => 2,
+            'id2' => 3
+        );
+
+        $expectedSql = 'UPDATE table SET column1 = :column1, column2 = :column2 WHERE id = :id AND id2 = :id2';
+
+        $connectionMock = $this->getMock('Afa\Database\IConnection');
+        $connectionMock->expects($this->once())
+            ->method('execute')
+            ->with($expectedSql, $arguments);
+
+        $command = new \Afa\Database\Command\Update('table', array(
+            'column1' => 1,
+            'column2' => 'a',
+        ), array(
+            'id' => 2,
+            'id2' => 3,
         ));
 
         $command->run($connectionMock);
